85294acf88e5251fef268baf25de582784fc513f
Sreshta29 <sreshtatadaka29@gmail.com>
2021-03-08 15:54:54 +0530

    new commit

--- welcome.sh
+++ welcome.sh
@@ -1,7 +1,11 @@
 echo "welcome to git"
 read name
 echo "hello $name"
+<<<<<<< HEAD
 echo "your first git command"
 echo "easy"
 echo "hello sreshta"
+echo "hello"
+=======
+>>>>>>> parent of 324a246... second commit
 
